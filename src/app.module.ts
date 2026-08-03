0cab4a4c9d7b01868b20ad6134dedddaa80e632b
luccas-sales <luccassales888@gmail.com>
2026-08-03 16:24:28 -0300

    tirando middleware para deixar apenas JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { SuppliersModule } from './suppliers/suppliers.module';
-import { Middleware } from './middleware';
 import { NotesRuralSuppliersModule } from './notes-rural-suppliers/notes-rural-suppliers.module';
 import { DailySalesModule } from './daily-sales/daily-sales.module';
 import { StoresModule } from './stores/stores.module';
@@ -20,14 +19,4 @@ import { AuthModule } from './auth/auth.module';
   controllers: [],
   providers: [PrismaService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(Middleware)
-      .exclude(
-        { path: 'updates/(.*)', method: RequestMethod.ALL },
-        { path: 'auth/signin', method: RequestMethod.POST },
-      )
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
