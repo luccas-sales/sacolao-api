f66db7ef39a81887cf824d3aa711b36c69661fa5
luccas-sales <luccassales888@gmail.com>
2026-08-03 10:48:19 -0300

    arrumando rota de atualização

--- src/releases/releases.controller.ts
+++ src/releases/releases.controller.ts
@@ -6,11 +6,11 @@ import type { Response } from 'express';
 export class ReleasesController {
   constructor(private readonly releasesService: ReleasesService) {}
 
-  @Get(['win32/:version', 'win32/:version/:file'])
+  @Get([':token/win32/:version', ':token/win32/:version/:file'])
   async getUpdate(
+    @Param('token') token: string,
     @Param('version') version: string,
     @Param('file') file: string,
-    @Query('token') token: string,
     @Res() res: Response,
   ) {
     return await this.releasesService.getUpdate(version, file, token, res);
